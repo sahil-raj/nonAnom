f71d24673bbf6ed2bf3313235e51bf7dfe56f75d
sahil-raj <sahilrajaaru@gmail.com>
2024-03-13 01:31:20 +0530

    enhancement

--- contracts/NonAnom.sol
+++ contracts/NonAnom.sol
@@ -18,25 +18,31 @@ contract NonAnom is ERC20 {
         userList[msg.sender] = getUid(_uid);
     }
 
+    modifier registeredCaller {
+        require(userList[msg.sender] != bytes32(0), "sender not found please register");
+        _;
+    }
+
+    modifier senderCaller(address _user) {
+        require(msg.sender == _user, "calling party not registering");
+        _;
+    }
+
     //function to create uid of an user using the hash(sha256) of the uid by the user and its wallet id
     function getUid(string memory _uid) public view returns(bytes32) {
         return keccak256(abi.encode(_uid, msg.sender));
     }
 
     //override the transfer function to make sure that uid must be there
-    function transfer(address to, uint256 value) public override returns (bool) {
-        //check if the sender is in userList
-        require(userList[msg.sender] != bytes32(0), "sender not found please register");
+    function transfer(address to, uint256 value) registeredCaller public override returns (bool) {
         //check if reciever is in userList
         require(userList[to] != bytes32(0), "reciever not found please ask them to register");
-        address owner = _msgSender();
-        _transfer(owner, to, value);
+        address from = _msgSender();
+        _transfer(from, to, value);
         return true;
     }
 
-    function registerUser(address _user, string memory _uid) public returns(bool) {
-        //check if person calling is the person registering
-        require(msg.sender == _user, "calling party not registering");
+    function registerUser(address _user, string memory _uid) senderCaller(_user) public returns(bool) {
         //register the user
         userList[_user] = getUid(_uid);
         return true;
